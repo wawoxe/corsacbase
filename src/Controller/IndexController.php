b7f42707672a45b37893ee0421820e795aba0605
Mykyta Melnyk <wawoxeq@gmail.com>
2023-12-03 16:16:13 +0100

    #11 Added and configured PHPInsights

--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -9,7 +9,7 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 #[Route(name: 'app_index_')]
-class IndexController extends AbstractController
+final class IndexController extends AbstractController
 {
     #[Route(
         path: '/',
