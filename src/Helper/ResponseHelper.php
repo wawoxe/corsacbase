dd3289d173ae18729b1f1f63857b77f55bff4227
Mykyta Melnyk <wawoxeq@gmail.com>
2023-12-19 22:03:06 +0100

    #24 Added ResponseHelper and maintenance type in .env, fixed mistype

---
+++ src/Helper/ResponseHelper.php
@@ -0,0 +1,37 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Helper;
+
+use InvalidArgumentException;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
+
+class ResponseHelper
+{
+    public const HTML_TYPE = 'html';
+    public const JSON_TYPE = 'json';
+
+    /**
+     * @var array<int, string> $types
+     */
+    private array $types = [self::HTML_TYPE, self::JSON_TYPE];
+
+    public function createResponse(string $type): Response
+    {
+        $this->responseTypeIsValid($type);
+
+        $response = $type === self::HTML_TYPE ? new Response() : new JsonResponse([]);
+        $response->setStatusCode(Response::HTTP_SERVICE_UNAVAILABLE);
+
+        return $response;
+    }
+
+    private function responseTypeIsValid(string $type): void
+    {
+        if (in_array($type, $this->types, true) === false) {
+            throw new InvalidArgumentException('[ResponseHelper] Response type is not valid.');
+        }
+    }
+}
